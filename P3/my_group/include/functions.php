16dbd9f46c0e67e10c859ebb3a4b7594381ffa3c
Dolores Maria Llidó Escrivá <dllido@MacBook-Pro-de-Dolores.local>
2018-10-18 13:34:56 +0200

    oct18

--- P3/my_group/include/functions.php
+++ P3/my_group/include/functions.php
@@ -22,6 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function my_group_install(){
     global $pdo;
     global $table;
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD); 
     $query="CREATE TABLE IF NOT EXISTS $table (person_id INT(11) NOT NULL AUTO_INCREMENT, nombre VARCHAR(100), apellidos VARCHAR(100), email VARCHAR(100),  foto_file VARCHAR(25), clienteMail VARCHAR(100)  PRIMARY KEY(client_id))";
     //echo $query;
     $pdo->exec($query);
