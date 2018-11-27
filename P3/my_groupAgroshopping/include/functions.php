0e1a90bba92c4a6969ffb1daea0d70de867e42df
Jorge Lengua <al341875@uji.es>
2018-11-27 10:15:43 +0000

    functions.php

--- P3/my_groupAgroshopping/include/functions.php
+++ P3/my_groupAgroshopping/include/functions.php
@@ -76,7 +76,9 @@ function hook_css() {
                border-collapse:collapse;
                }
             td,th{
-                border: 7px solid black;
+                border: 1px solid black;
+                font-family:Arial, sans-serif;
+                font-size:14px;
             }
 
            }
