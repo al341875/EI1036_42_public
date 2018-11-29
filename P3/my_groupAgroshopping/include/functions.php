5db380c9db142414b6f0498f312111b3b7a1e0ae
Jorge Lengua <al341875@uji.es>
2018-11-29 18:31:14 +0000

    functions.php

--- P3/my_groupAgroshopping/include/functions.php
+++ P3/my_groupAgroshopping/include/functions.php
@@ -94,9 +94,9 @@ function AS_MP_my_datos()
         case "registrar":
             //$url33='/mnt/data/vhosts/casite-1006648.cloudaccess.net/httpdocs/FOTOS';
             $fotoURL='';
-            $IMAGENES_USUARIOS='/mnt/data/vhosts/casite-1006648.cloudaccess.net/httpdocs/FOTOS/';
+            $IMAGENES_USUARIOS='/mnt/data/vhosts/casite-1006648.cloudaccess.net/httpdocs/Lab/P1/img/';
             $URL2 = '';
-            $location = '/FOTOS/';
+            $location = '/Lab/P1/img/';
             if(array_key_exists('ffile', $_FILES) && $_POST['email']) {
             $fotoURL = sanitize_text_field($IMAGENES_USUARIOS.$_POST['userName']."_".$_FILES['ffile']['name']);
             $URL2 = sanitize_text_field($location.$_POST['userName']."_".$_FILES['ffile']['name']);
