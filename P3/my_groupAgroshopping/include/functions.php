ca7ff361ac7e1e9e284d664187d21de1645f9669
Jorge Lengua <al341875@uji.es>
2018-12-12 19:44:20 +0000

    AS

--- P3/my_groupAgroshopping/include/functions.php
+++ P3/my_groupAgroshopping/include/functions.php
@@ -34,7 +34,7 @@ function AS_MP_Register_Form($MP_user , $user_email)
 {//formulario registro amigos de $user_email
     ?>
     <h1>Gestión de Usuarios </h1>
-    <form class="fom_usuario" action="?action=my_datos&proceso=registrar_as" method="POST"  enctype="multipart/form-data">
+    <form class="fom_usuario" action="?action=AS_MP_my_datos&proceso=registrar_as" method="POST"  enctype="multipart/form-data">
         <label for="clienteMail">Tu correo</label>
         <br/>
         <input type="text" name="clienteMail"  size="20" maxlength="25" value="<?php print $user_email?>"
@@ -114,7 +114,7 @@ function AS_MP_my_datos()
             $consult = $MP_pdo_AS->prepare($query);
             $a=$consult->execute($a);
             if (1>$a) {echo "InCorrecto $query";}
-            else wp_redirect(admin_url( 'admin-post.php?action=my_datos&proceso=listar_as'));
+            else wp_redirect(admin_url( 'admin-post.php?action=AS_MP_my_datos&proceso=listar_as'));
             break;
         case "listar_as":
             //Listado amigos o de todos si se es administrador.
