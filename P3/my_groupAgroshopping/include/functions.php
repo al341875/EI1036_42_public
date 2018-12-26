4fca90b45c482566a872969b2eb0d2974015f770
Jorge Lengua <al341875@uji.es>
2018-12-26 11:10:16 +0100

    P5

--- P3/my_groupAgroshopping/include/functions.php
+++ P3/my_groupAgroshopping/include/functions.php
@@ -31,70 +31,6 @@ function MP_CrearAS($table){
     $consult->execute (array());
 }
 
-
-
-function MP_Register_FormAS($MP_userAS , $user_email)
-{//formulario registro amigos de $user_email
-    ?>
-
-
-    <h1>GestiÓn de Usuarios </h1>
-    <form class="form" action="?action=my_datos&proceso=registrar" method="POST" enctype="multipart/form-data">
-        <label for="clienteMail">Tu correo</label>
-        <br/>
-        <input type="text" name="clienteMail"  size="20" maxlength="25" value="<?php print $user_email?>"
-        readonly />
-        <br/>
-        <legend>Datos básicos</legend>
-<br/>
-        <label for="nombre">Nombre</label>
-        <br/>
-        <input type="text" name="userName" class="item_requerid" size="20" maxlength="25" value="<?php print $MP_userAS["userName"] ?>"
-        placeholder="Miguel Cervantes" />
-        <br/>
-        <label for="email">Email</label>
-        <br/>
-        <input type="text" name="email" class="item_requerid" size="20" maxlength="25" value="<?php print $MP_userAS["email"] ?>"
-        placeholder="kiko@ic.es" />
-        <br/>
- <label for="foto_file">Foto</label>
-<br/>
-<input type="file" id="foto" name="foto" class="item_requerid" value="<?php print $foto ?>" required />
-<br/>
-<br>
-<p> <img id="img_foto" src="" width="100" height="60"></p>
-
-        <input type="submit" value="Enviar">
-        <input type="reset" value="Deshacer">
-    </form>
-
-
-<script type="text/javascript" charset="utf-8">
-
-      function mostrarFoto(file, imagen) {
-	  //carga la imagen de file en el elemento src imagen
-         var reader = new FileReader();
-         reader.addEventListener("load", function () {
-            imagen.src = reader.result;
-         });
-         reader.readAsDataURL(file);
-      }
-
-      function ready() {
-         var fichero = document.querySelector("#foto");
-         var imagen  = document.querySelector("#img_foto");
-	  //escuchamos evento selección nuevo fichero.
-         fichero.addEventListener("change", function (event) {
-            mostrarFoto(this.files[0], imagen);
-         });
-      }
-
-      ready();
-
-   </script>
-<?php
-}
-
 function MP_Update_Form_AS($MP_userAS , $user_email,$nom,$email,$foto,$person_id)
 {//formulario registro amigos de $user_email
 
@@ -240,20 +176,8 @@ function MP_my_datosAS()
             else wp_redirect(admin_url( 'admin-post.php?action=my_datos&proceso=listar'));
             break;
 case "update":
-$table2='A_GrupoCliente';
-			$valor=$_GET["person_id"];
-			$query = "SELECT     * FROM       $table WHERE person_id=$valor "; 
-			$consult = $MP_pdo_AS->prepare($query);
-				$a=$consult->execute(array());
-				//if (1>$a)echo "InCorrecto2";
-				$result=($consult->fetch(PDO::FETCH_ASSOC));
-			//var_dump($result);
-			$MP_userAS=null; //variable a rellenar cuando usamos modificar con este formulario
-			$nom= $result["nombre"];
-			$email= $result["email"];
-            $foto= $result["foto_file"];
-            
-			MP_pdo_AS($MP_userAS,$user_email,$nom,$email,$foto,$valor);
+include_once(plugin_dir_path(__FILE__) . '../templates/update.php');
+
 			break;
 case "updatear":
 $fotoURL="";
